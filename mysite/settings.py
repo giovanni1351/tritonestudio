7b27ed1b423b539d05954717404625e94babc69d
Giovanni Chahin Morassi <giovannimorassi13511@gmail.com>
2025-02-23 15:19:32 -0300

    Arrumando o bagui aqui

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except Exception as e:
+    print(e)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
